37cceec34c6c182ce563a356bab1341ce0609f3b
DmitriyKotov1986 <ikot@mail.ru>
2021-09-21 18:33:22 +0600

    Первычный коммит

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <QCoreApplication>
+#include <QTimer>
+#include <QCommandLineParser>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    QCoreApplication::setApplicationName("SystemInfo");
+    QCoreApplication::setOrganizationName("OOO SA");
+    QCoreApplication::setApplicationVersion("0.1");
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription("Program for gettin system information");
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    QCommandLineOption Interval(QStringList() << "interval", "Interval beetwen getting information", "IntervalValue", "0");
+    parser.addOption(Interval);
+
+    parser.process(a);
+
+    bool isExecutable = parser.isSet(Interval);
+    if (isExecutable) {
+      qDebug() << parser.value(Interval);
+    }
+    else {
+      qDebug() << "asdfasfasfas";
+    }
+
+   // QTimer *Timer = new QTimer();
+
+  //  a.
+
+
+//    Timer->setInterval()
+
+
+    return a.exec();
+}
